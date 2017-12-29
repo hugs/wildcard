2c5bfa8263433dd19b596fa5bfa09938e7dd8e01
Jason Huggins <jrhuggins@gmail.com>
2017-12-29 09:09:57 -0600

    Whitespace fixes

--- test/invalid_programs.js
+++ test/invalid_programs.js
@@ -1,23 +1,23 @@
 const invalid = [
-  '10 ⏹️',		// => Missing ▶️ before "10" at 1:4
-	'1️⃣0️⃣⏹️',		// => Missing ▶️ before "1️⃣0️⃣" at 1:5
-  '⬜⏹️',		 // => Missing ▶️ before "⬜" at 1:2
-	'▶️', 			 // => Expected STOP, got null at 1:4
-	'⏹️', 			// => Unexpected token: ⏹️ (at 1:1)
-	'▶️👋🏽',   	 // => Expected STOP, got null at 1:10
-	'▶️👋🏽🌐',   // => Expected STOP, got null at 1:10
-	'👋🏽🌐⏹️',   // => Missing ▶️ before "👋🏽🌐" at 1:7
-	':say',   	// => Unexpected input: :say at (1:1)
-	'say:',   	// => Unexpected input: say: at (1:1)
-  'yo', 			// => Unexpected input: yo at (1:1)
+  '10 ⏹️',    // => Missing ▶️ before "10" at 1:4
+  '1️⃣0️⃣⏹️',     // => Missing ▶️ before "1️⃣0️⃣" at 1:5
+  '⬜⏹️',     // => Missing ▶️ before "⬜" at 1:2
+  '▶️',        // => Expected STOP, got null at 1:4
+  '⏹️',       // => Unexpected token: ⏹️ (at 1:1)
+  '▶️👋🏽',      // => Expected STOP, got null at 1:10
+  '▶️👋🏽🌐',    // => Expected STOP, got null at 1:10
+  '👋🏽🌐⏹️',   // => Missing ▶️ before "👋🏽🌐" at 1:7
+  ':say',     // => Unexpected input: :say at (1:1)
+  'say:',     // => Unexpected input: say: at (1:1)
+  'yo',       // => Unexpected input: yo at (1:1)
   'hello world',
- 	"💬 'There are no rules here--we're trying to accomplish something'",
-	"▶️'💡'💡'⏹️",
-	'⏹️"' + '!' + "שלום עולם" + '"💬',		// Hebrew
-	//'🌐👋🏽◀️',
-	'⏹️◀️',
-	'▶️⏹️◀️',
-	'▶️⏹️',
+  "💬 'There are no rules here--we're trying to accomplish something'",
+  "▶️'💡'💡'⏹️",
+  '⏹️"' + '!' + "שלום עולם" + '"💬',    // Hebrew
+  //'🌐👋🏽◀️',
+  '⏹️◀️',
+  '▶️⏹️◀️',
+  '▶️⏹️',
 ]
 
 module.exports = invalid
